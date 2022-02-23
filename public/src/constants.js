54e152d52ca6c299528790a160f525cc87593c1c
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-02-23 14:37:15 +0900

    fix: heroku server url

--- public/src/constants.js
+++ public/src/constants.js
@@ -1,7 +1,7 @@
 const LOCAL_FETCH_URL = "http://127.0.0.1:5500/back/data/";
 const SERVER_FETCH_URL = "http://127.0.0.1:3000/";
 const GITHUB_PAGE_FETCH_URL = "https://healtheloper.github.io/fe-kakaopage/";
-const HEROKU_SERVER_URL = "http://fe-kakaopage-back.herokuapp.com/";
+const HEROKU_SERVER_URL = "https://fe-kakaopage-back.herokuapp.com/";
 
 export {
   LOCAL_FETCH_URL,
